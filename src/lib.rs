c5b6abd01d09802e65567f35d431a8289a26a980
Ji Chen <jaykchen@gmail.com>
2023-07-26 22:17:25 -0400

    @20

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ use web_scraper_flows::get_page_text;
 #[tokio::main(flavor = "current_thread")]
 pub async fn run() {
     schedule_cron_job(
-        String::from("30 * * * *"),
+        String::from("20 * * * *"),
         String::from("cronjob scheduled"),
         callback,
     )
